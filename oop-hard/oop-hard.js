ce181de53df20470d073652c234511c6e8f49301
agungcaproex <agung.caproex@gmail.com>
2018-03-15 10:58:43 +0700

    titip oop release 1

--- oop-hard/oop-hard.js
+++ oop-hard/oop-hard.js
@@ -1,4 +1,4 @@
-class Heroes{
+class rikimarues{
     constructor(name, job, health, mana, attack, money) {
         this._name = name
         this._job = job
@@ -26,31 +26,37 @@ class Heroes{
         this._mana -= item._manapoint
         this._attack -= item._attackpoint
         this._money += item._price*(50/100)
-        
+        for(let i=0; i<this._item.length; i++){
+            if(this._item[0]._name == item._name){
+                let index = this._item.indexOf(item._name)
+                this._item.splice(index, 1)
+                // console.log(this._item)
+            }
+        }
     }
     
     
 }
 
-class Rikimaru extends Heroes{
+class Rikimaru extends rikimarues{
     constructor() {
         super('Rikimaru', 'Assassin', 1200, 543, 431, 1200)
     }
 }
 
-class Leonidas extends Heroes{
+class Leonidas extends rikimarues{
     constructor() {
         super('Leonidas', 'Knight', 3213, 126, 431, 1700)
     }
 }
 
-class Gandalf extends Heroes{
+class Gandalf extends rikimarues{
     constructor() {
         super('Gandalf', 'Mage', 1130, 603, 231, 2500)
     }
 }
 
-class Ezio extends Heroes{
+class Ezio extends rikimarues{
     constructor() {
         super('Ezio', 'Assassin', 1250, 523, 431, 2100)
     }
@@ -68,20 +74,34 @@ class Items{
     }
 }
 
+class Monster{
+    constructor(name, health, attack, weakness) {
+        this._name = name
+        this._health = health
+        this._attack = attack
+        this._weakness = weakness
+    }
+}
 
 
-let hero = new Rikimaru()
-console.log(hero)
+
+let rikimaru = new Rikimaru()
+let leonidas = new Leonidas()
+let gandalf = new Gandalf()
+let ezio = new Ezio()
+console.log(rikimaru)
 
 
 let pedang = new Items('Pedang',['Assassin','Knight'], 'Pedang Warisan Dewa', 800, 400, 1000, 900)
 let tongkat = new Items('Tongkat', ['Mage'], 'Tongkat Sakti Harry Potter', 2200, 600, 200, 1500)
 // console.log(pedang)
+let monster1 = new Monster('Gorgom', 800, 400, 'Assasin')
+let monster2 = new Monster('Gintama', 1000, 300, 'Mage')
 
 
-console.log('=================')
-hero.buyItem(pedang)
-console.log(hero)
-console.log('=================')
-hero.sellItem(pedang)
-console.log(hero)
+console.log('====SETELAH BELI ITEM')
+rikimaru.buyItem(pedang)
+console.log(rikimaru)
+console.log('====SETELAH JUAL ITEM======')
+// rikimaru.sellItem(pedang)
+// console.log(rikimaru)
