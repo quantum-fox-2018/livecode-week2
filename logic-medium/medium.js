5573a1404a53718e94e118206dbd07649651590d
Fitrul Islam <fitrul.islam@gmail.com>
2018-03-15 11:18:47 +0700

    logic revised

--- logic-medium/medium.js
+++ logic-medium/medium.js
@@ -12,16 +12,16 @@ function generateAlphabetBoard(num){
   return board
 }
 
-function checkConsonantInBox(x,y,num){
+function checkConsonantInBox(y,x,num){
   let myBoard = generateAlphabetBoard(num)
   console.log(myBoard)
   console.log(`Box in column ${x} and row ${y}`);
   let row = Math.floor(y/num)*num
   let col = Math.floor(x/num)*num
-  console.log(row);
-  console.log(col);
+  console.log(`Start Row = ${row}`);
+  console.log(`Start Col = ${col}`);
   for(let i=row; i<row+num; i++){
-    for(let j=row; j<col+num; j++){
+    for(let j=col; j<col+num; j++){
       if(myBoard[i][j]=='a' || myBoard[i][j]=='i' || myBoard[i][j]=='u' || myBoard[i][j]=='e' || myBoard[i][j]=='o'){
         return false
       }
@@ -32,5 +32,5 @@ function checkConsonantInBox(x,y,num){
 
 var grid = 3
 // console.log(checkConsonantInBox(4,3,grid));
-console.log(checkConsonantInBox(5,7,grid));
-// console.log(checkConsonantInBox(1,2,grid));
+// console.log(checkConsonantInBox(7,5,grid));
+console.log(checkConsonantInBox(1,2,grid));
