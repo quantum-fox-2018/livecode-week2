6d8e8de08fa50bc44d90be11db7951d8f11a8a8f
Natascha Lamsu <tazcha15@gmail.com>
2018-03-15 11:41:46 +0700

    oop hard live code w2

--- oop-hard/rpg.js
+++ oop-hard/rpg.js
@@ -1,40 +1,93 @@
-class Hero {
- constructor(name, health, mana, defense, money) {
+class Heroes {
+ constructor(name, health, mana, attack, money) {
    this.name = name;
-   this.job = job;
    this.health = health;
    this.mana = mana;
    this.attack = attack;
    this.money = money;
-   this.items = items;
  }
 }
 
-class Assassin extends Hero {
-  contructor(name, health, mana, attack, money) {
-    super(name, health, mana, attack, money)
-    this.skill = 'Ciat..! Serangan tanpa bayangan..';
-    this.job = 'Assassin';
-  }
-}
-
-class Knight extends Hero {
-  contructor(name, health, mana, attack, money) {
-    super(name, health, mana, attack, money)
-    this.skill = 'Lemparan Perisai Suci';
-    this.job = 'Knight';
-  }
-}
-
-class Mage extends Hero {
-  contructor(name, health, mana, attack, money) {
+class Assassin extends Heroes {
+  contructor(name, health, mana, attack, money, job) {
     super(name, health, mana, attack, money);
-    this.skill = 'Terimalah serangan sihir ini..';
-    this.job = 'Mage';
+    this.skill = heroSkill();
+    this.job = job;
+  }
+
+  heroSkill() {
+    return 'Ciat..! Serangan tanpa bayangan..';
   }
 }
 
-class Character{
+class Knight extends Heroes {
+  contructor(name, health, mana, attack, money, job) {
+    super(name, health, mana, attack, money);
+    this.skill = heroSkill();
+    this.job = job;
+  }
+
+  heroSkill() {
+    return 'Lemparan Perisai Suci';
+  }
+}
+
+class Mage extends Heroes {
+  contructor(name, health, mana, attack, money, job) {
+    super(name, health, mana, attack, money);
+    this.skill = heroSkill();
+    this.job = job;
+  }
+
+  heroSkill() {
+    return 'Terimalah serangan sihir ini..';
+  }
+}
+
+class Items {
+  constructor(itemName, typeHero, price, healthPoint, manaPoint, attackPoint) {
+    this.itemName = itemName;
+    this.typeHero = typeHero;
+    this.itemPrice = price;
+    this.healthPoint = healthPoint;
+    this.manaPoint = manaPoint;
+    this.defensePoint = defensePoint;
+  }
+}
+
+class Pedang extends Items {
+  constructor(itemName, heroJob, price, healthPoint, manaPoint, attackPoint) {
+    super(itemName, heroJob, price, healthPoint, manaPoint, attackPoint)
+  }
+}
+
+class Perisai extends Items {
+  constructor(itemName, heroJob, price, healthPoint, manaPoint, attackPoint) {
+    super(itemName, heroJob, price, healthPoint, manaPoint, attackPoint)
+  }
+}
+
+class Monsters {
+  constructor(monsterName, monsterHealth, monsterAttack, monsterWeakness) {
+    this.monsterName = monsterName;
+    this.monsterHealth = monsterHealth;
+    this.monsterAttack = monsterWeakness;
+  }
+}
+
+class KingCrocs extends Monsters {
+  constructor(monsterName, monsterHealth, monsterAttack, monsterWeakness) {
+    super(monsterName, monsterHealth, monsterAttack, monsterWeakness)
+  }
+}
+
+class Dormouse extends Monsters {
+  constructor(monsterName, monsterHealth, monsterAttack, monsterWeakness) {
+    super(monsterName, monsterHealth, monsterAttack, monsterWeakness)
+  }
+}
+
+class GameInfo {
   constructor() {
     this.heroes = [];
     this.items = [];
@@ -48,37 +101,34 @@ class Character{
     this.items.push(item);
   }
 
-  // buyItem(item, money) {
-  //   for(var i = 0; i < this.items.length; i++) {
-  //     if(this.items[i].job)
-  //   }
-  // }
-}
+  buyItem(hero, item, money) {
+    canBuy = false;
+    for(var i = 0; i < this.items.length; i++) {
+      for(var j = 0; j < this.heroes.length; j ++) {
+        if(this.heroes[j].name === hero && this.items[i].heroJob === this.heroes[j].job) {
+          canBuy = true;
+        }
+      }
+    }
+  }
 
-class Item {
-  constructor(itemName, heroJob, price, healthPoint, manaPoint, attackPoint) {
-    this.itemName = itemName;
-    this.heroJob = heroJob;
-    this.itemPrice = price;
-    this.healthPoint = healthPoint;
-    this.manaPoint = manaPoint;
-    this.defensePoint = defensePoint;
+  sellItem() {
+
   }
 }
 
-class Pedang extends Item {
-  constructor(itemName, heroJob, price, healthPoint, manaPoint, attackPoint) {
-    super(itemName, heroJob, price, healthPoint, manaPoint, attackPoint)
-  }
-}
 
-var mage = new Mage('Gandalf', 1130, 603, 231, 2500);
-var knight = new Knight('Leonidas', 3213, 126, 431, 1700);
-var assassin = new Assassin('Rikimaru', 1200, 543, 431, 1200);
-var chars = new Character();
-var item = Item.heroItem('pedang', 'Assassin', 200, 1000, 550, 345);
-// chars.addItem(item);
-chars.addHero(mage);
-// chars.addHero(knight);
-// chars.addHero(assassin);
-console.log(chars);
+
+var mage = new Mage('Gandalf', 1130, 603, 231, 2500, 'Mage');
+var knight = new Knight('Leonidas', 3213, 126, 431, 1700, 'Knight');
+var assassin = new Assassin('Rikimaru', 1200, 543, 431, 1200, 'Assassin');
+var gameInfo = new gameInfo();
+// var pedang = new Pedang('pedang', 'Assassin', 200, 1000, 550, 345);
+// var perisai = new Perisai('perisai', 'Assassin', 200, 1000, 550, 345);
+
+gameInfo.addHero(mage);
+gameInfo.addHero(knight);
+gameInfo.addHero(assassin);
+// gameInfo.addItem(pedang);
+// gameInfo.addItem(perisai);
+console.log(gameInfo);
